e143406679d5913ece7ccf625c924e4841f797d5
hcrispim <hcrispim@gmail.com>
2024-03-29 19:01:18 -0300

    nginx e ssh

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class AdminController extends Controller
+{
+    public function AdminDashboard()
+    {
+        return view('admin.index');
+    } // End Method
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect('admin/login')->with($notification);;
+    }
+
+    public function AdminLogin()
+    {
+        return view('admin.admin_login');
+    } // End Method
+
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    } // End Method
+
+    public function AdminProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/' . $data->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    } // End Method
+    public function AdminChangePassword(){
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_change_password',compact('profileData'));
+    }// End Method
+
+    public function AdminUpdatePassword(Request $request){
+        //Validate
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        //Match old password
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            $notification = array(
+                'message' => 'Old password does not match',
+                'alert-type' => 'error'
+            );
+            return back()->with($notification);
+        }
+
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+        return back()->with($notification);
+    }// End Method
+
+    /////////// Agent User All Method ////////////
+
+    public function AllAgent(){
+        $allagent = User::where('role','agent')->get();
+        return view('backend.agentuser.all_agent',compact('allagent'));
+    }// End Method
+
+}
---
+++ routes/web.php
@@ -0,0 +1,124 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
+use App\Http\Controllers\Backend\PropertyController;
+use App\Http\Controllers\Backend\PropertyTypeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [UserController::class, 'Index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user/profile', [UserController::class, 'UserProfile'])->name('user.profile');
+    Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
+    Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout');
+    Route::get('/user/change/password', [UserController::class, 'UserChangePassword'])->name('user.change.password');
+    Route::post('/user/password/update', [UserController::class, 'UserPasswordUpdate'])->name('user.password.update');
+});
+
+require __DIR__.'/auth.php';
+
+//Admin Middleware
+Route::middleware(['auth','role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin.update.password', [AdminController::class, 'AdminUpdatePassword'])->name('admin.update.password');
+});
+
+/// Agent Group Middleware
+Route::middleware(['auth','role:agent'])->group(function(){
+    Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+    Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agent.logout');
+    Route::get('/agent/profile', [AgentController::class, 'AgentProfile'])->name('agent.profile');
+    Route::post('/agent/profile/store', [AgentController::class, 'AgentProfileStore'])->name('agent.profile.store');
+    Route::get('/agent/change/password', [AgentController::class, 'AgentChangePassword'])->name('agent.change.password');
+    Route::post('/agent/update/password', [AgentController::class, 'AgentUpdatePassword'])->name('agent.update.password');
+}); // End Group Agent Middleware
+
+//Use Middleware
+Route::middleware(['auth','role:user'])->group(function () {
+   Route::get('/user/dashboard', [UserController::class, 'UserDashboard'])->name('user.dashboard');
+});
+
+
+Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
+Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware(RedirectIfAuthenticated::class);
+
+Route::controller(PropertyTypeController::class)->group(function(){
+    Route::get('/all/type', 'AllType')->name('all.type');
+    Route::get('/add/type', 'AddType')->name('add.type');
+    Route::post('/store/type', 'StoreType')->name('store.type');
+    Route::get('/edit/type/{id}', 'EditType')->name('edit.type');
+    Route::post('/update/type', 'UpdateType')->name('update.type');
+    Route::get('/delete/type/{id}', 'DeleteType')->name('delete.type');
+});
+
+// Amenities Type All Route
+Route::controller(PropertyTypeController::class)->group(function () {
+    Route::get('/all/amenitie', 'AllAmenitie')->name('all.amenitie');
+    Route::get('/add/amenitie', 'AddAmenitie')->name('add.amenitie');
+    Route::post('/store/amenitie', 'StoreAmenitie')->name('store.amenitie');
+    Route::get('/edit/amenitie/{id}', 'EditAmenitie')->name('edit.amenitie');
+    Route::post('/update/amenitie', 'UpdateAmenitie')->name('update.amenitie');
+    Route::get('/delete/amenitie/{id}', 'DeleteAmenitie')->name('delete.amenitie');
+});
+
+// Property All Route
+Route::controller(PropertyController::class)->group(function(){
+
+    Route::get('/all/property', 'AllProperty')->name('all.property');
+    Route::get('/add/property', 'AddProperty')->name('add.property');
+    Route::post('/store/property', 'StoreProperty')->name('store.property');
+    Route::get('/edit/property/{id}', 'EditProperty')->name('edit.property');
+    Route::post('/update/property', 'UpdateProperty')->name('update.property');
+    Route::post('/update/property/thambnail', 'UpdatePropertyThambnail')->name('update.property.thambnail');
+    Route::post('/update/property/multiimage', 'UpdatePropertyMultiimage')->name('update.property.multiimage');
+    Route::get('/property/multiimg/delete/{id}', 'PropertyMultiImageDelete')->name('property.multiimg.delete');
+    Route::post('/store/new/multiimage', 'StoreNewMultiimage')->name('store.new.multiimage');
+    Route::post('/update/property/facilities', 'UpdatePropertyFacilities')->name('update.property.facilities');
+    Route::get('/delete/property/{id}', 'DeleteProperty')->name('delete.property');
+    Route::get('/details/property/{id}', 'DetailsProperty')->name('details.property');
+    Route::post('/inactive/property', 'InactiveProperty')->name('inactive.property');
+    Route::post('/active/property', 'ActiveProperty')->name('active.property');
+    Route::get('/admin/package/history', 'AdminPackageHistory')->name('admin.package.history');
+    Route::get('/package/invoice/{id}', 'PackageInvoice')->name('package.invoice');
+    Route::get('/admin/property/message/', 'AdminPropertyMessage')->name('admin.property.message');
+});
+
+// Agent All Route from admin
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/all/agent', 'AllAgent')->name('all.agent');
+    Route::get('/add/agent', 'AddAgent')->name('add.agent');
+    Route::post('/store/agent', 'StoreAgent')->name('store.agent');
+    Route::get('/edit/agent/{id}', 'EditAgent')->name('edit.agent');
+    Route::post('/update/agent', 'UpdateAgent')->name('update.agent');
+    Route::get('/delete/agent/{id}', 'DeleteAgent')->name('delete.agent');
+    Route::get('/changeStatus', 'changeStatus');
+
+});
